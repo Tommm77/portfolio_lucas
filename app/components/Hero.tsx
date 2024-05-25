409012990c4dfba82e54d63578c2db677007442b
Tommm77 <picout.tom@gmail.com>
2024-05-25 17:09:40 +0200

    add medium icon

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -41,7 +41,7 @@ export const Hero = () => {
                 <div className="flex flex-row space-x-2">
                     <button type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                        <CustomIcon name={"github"} size={24}/>
+                        <CustomIcon name={"medium"} size={24}/>
                     </button>
                     <button type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out">
