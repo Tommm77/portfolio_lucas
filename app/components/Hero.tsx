49da0b32d9a29fb0e3601496a248ae0f18efef48
Tommm77 <picout.tom@gmail.com>
2024-05-25 22:57:05 +0200

    Fix width of hero title

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -30,7 +30,7 @@ export const Hero = () => {
                 </AnimatedGradientText>
                 <AnimatedGradientText>
                     <span
-                        className="animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-center h-24 sm:h-32 -mt-5 sm:-mt-10">CyberSecurity & Cloud Engineer</span>
+                        className="animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-center -mt-5 sm:-mt-10">CyberSecurity & Cloud Engineer</span>
                 </AnimatedGradientText>
 
                 <p className="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 font-semibold text-center text-sm sm:text-base md:text-lg mb-5">
