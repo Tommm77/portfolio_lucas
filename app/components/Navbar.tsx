fd0c6a441cab44c2bc98e2feb7e732b766525c9f
Tommm77 <picout.tom@gmail.com>
2024-05-25 17:18:07 +0200

    navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,21 @@
-import {Section} from "@/app/components/Section";
-import {Button} from "@/components/ui/button";
+import { Section } from "@/app/components/Section";
 
 export const Navbar = () => {
     return (
         <Section>
-            <nav className="flex justify-between items-center py-4 px-72">
+            <nav className="flex justify-between items-center py-4 px-4 md:px-8 lg:px-16 xl:px-72 w-full">
                 <div className="flex items-center">
-                    <a href="/" className="text-2xl font-bold">Lucas</a>
+                    <a href="/" className="text-xl md:text-2xl font-bold">Lucas</a>
                 </div>
                 <div className="flex items-center">
-                    <button type="button"
-                            className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out">
+                    <button
+                        type="button"
+                        className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 text-center uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out"
+                    >
                         Contact Me
                     </button>
                 </div>
             </nav>
         </Section>
-    )
-}
+    );
+};
