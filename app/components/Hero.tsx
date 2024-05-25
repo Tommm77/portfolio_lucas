86bfb5f0997ec2287783fb1c3ab356c8d2b43460
Tommm77 <picout.tom@gmail.com>
2024-05-25 17:23:59 +0200

    reponsive hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,47 +1,40 @@
-import {Section} from "@/app/components/Section";
-import {CustomIcon} from "@/app/components/icons/Icons";
-import {Button} from "@/components/ui/button";
+import { Section } from "@/app/components/Section";
+import { CustomIcon } from "@/app/components/icons/Icons";
 import AnimatedGradientText from "@/app/components/magicui/animated-gradient-text";
-import {cn} from "@/lib/utils";
 import WordRotate from "@/app/components/magicui/word-rotate";
-import RadialGradient from "@/app/components/magicui/radial-gradient";
 
 export const Hero = () => {
     return (
         <Section className="relative">
-            <div className="flex flex-col items-center justify-center mt-20">
+            <div className="flex flex-col items-center justify-center mt-20 px-4 md:px-8">
                 <AnimatedGradientText>
                     <WordRotate
-                        className="animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-8xl text-center h-32"
+                        className="animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-center h-12 sm:h-32"
                         words={[
                             "Security Expert.",
                             "Brilliant Assessor.",
-                            "Exceptional Responder.",
                             "Innovative Manager.",
                             "Skilled Defender.",
-                            "Dedicated Cryptographer.",
                             "Proficient Analyst.",
                             "Outstanding Tester.",
                             "Talented Detector.",
                             "Resilient Protector.",
-                            "Experienced Policy Maker.",
-                            "Remarkable Defender.",
                         ]}
                     />
                 </AnimatedGradientText>
                 <AnimatedGradientText>
                     <span
-                        className="animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-8xl text-center h-32 -mt-10">CyberSecurity Engineer</span>
+                        className="animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-center h-24 sm:h-32 -mt-5 sm:-mt-10">CyberSecurity Engineer</span>
                 </AnimatedGradientText>
 
-                <p className="w-1/3 font-semibold text-center text-lg mb-5">Hi 👋. My name is Lucas, and I am a
-                    cybersecurity engineer living in Paris & working remotely with the whole world.
+                <p className="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 font-semibold text-center text-sm sm:text-base md:text-lg mb-5">
+                    Hi 👋. My name is Lucas, and I am a cybersecurity engineer living in Paris & working remotely with the whole world.
                 </p>
 
-                <div className="flex flex-row space-x-2">
+                <div className="flex flex-row">
                     <button type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                        <CustomIcon name={"medium"} size={24}/>
+                        <CustomIcon name={"medium"} size={24} />
                     </button>
                     <button type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out">
@@ -49,7 +42,7 @@ export const Hero = () => {
                     </button>
                     <button type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                        <CustomIcon name={"linkedin"} size={24}/>
+                        <CustomIcon name={"linkedin"} size={24} />
                     </button>
                 </div>
             </div>
