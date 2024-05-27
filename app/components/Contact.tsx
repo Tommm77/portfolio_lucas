692a02ad2a359c82ab875b18f3358351fec48db7
Tommm77 <picout.tom@gmail.com>
2024-05-27 20:09:31 +0200

    add link

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -14,26 +14,28 @@ export const Contact = () => {
                     <div className="flex flex-col items-center space-y-4 w-full md:w-1/6">
                         <h3 className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">Medium</h3>
                         <p className="text-center text-lg">LCS</p>
-                        <button
+                        <a
                             type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out"
+                            href="https://medium.com/@swzwnc"
                         >
                             See Posts
-                        </button>
+                        </a>
                     </div>
                     <div className="flex flex-col items-center space-y-4 w-full md:w-1/6">
                         <h3 className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">RootMe</h3>
-                        <p className="text-center text-lg">Lcs</p>
-                        <button
+                        <p className="text-center text-lg">LCS</p>
+                        <a
                             type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out"
+                            href="https://www.root-me.org/LCS?lang=fr#60d6cfa6b6845f8f28c1f44001816270"
                         >
-                            Visit Profile
-                        </button>
+                            Check Profile
+                        </a>
                     </div>
                     <div className="flex flex-col items-center space-y-4 w-full md:w-1/6">
                         <h3 className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">HackTheBox</h3>
-                        <p className="text-center text-lg">Lucas Offredi</p>
+                        <p className="text-center text-lg">LCS6</p>
                         <button
                             type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out"
@@ -43,13 +45,14 @@ export const Contact = () => {
                     </div>
                     <div className="flex flex-col items-center space-y-4 w-full md:w-1/6 relative">
                         <h3 className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">Linkedin</h3>
-                        <p className="text-center text-lg">Lucas Offredi</p>
-                        <button
+                        <p className="text-center text-lg">Lucas O</p>
+                        <a
                             type="button"
                             className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out"
+                            href="https://www.linkedin.com/in/lucas-o-a34971243/"
                         >
                             Visit Profile
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>
