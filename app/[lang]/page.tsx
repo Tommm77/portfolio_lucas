53ac1d378fdd081430efbf4913855727c64b4aa7
Tommm77 <picout.tom@gmail.com>
2024-05-28 12:28:31 +0200

    fix page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,4 +1,3 @@
-import { notFound } from 'next/navigation';
 import { Navbar } from "@/app/components/Navbar";
 import { Hero } from "@/app/components/Hero";
 import { Projects } from "@/app/components/Projects";
@@ -6,15 +5,8 @@ import { About } from "@/app/components/About";
 import { Stack } from "@/app/components/Stack";
 import { Contact } from "@/app/components/Contact";
 import { Footer } from "@/app/components/Footer";
-import { useTranslations } from 'next-intl';
 
-export default function Home({ params }: { params: { lang: string } }) {
-    const t = useTranslations('Home');
-
-    if (!params.lang) {
-        notFound();
-    }
-
+export default function Home() {
     return (
         <main>
             <Navbar />
@@ -26,13 +18,4 @@ export default function Home({ params }: { params: { lang: string } }) {
             <Footer />
         </main>
     );
-}
-
-export async function generateStaticParams() {
-    return [{ lang: 'en' }, { lang: 'fr' }];
-}
-
-export async function getMessages(locale: string) {
-    const messages = (await import(`../../messages/${locale}.json`)).default;
-    return messages;
 }
