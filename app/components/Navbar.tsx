08ed5d2d51bf0ec29f0286c0faa6d6745e0adc8b
Tommm77 <picout.tom@gmail.com>
2024-05-28 12:29:37 +0200

    remove contact from navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -32,13 +32,6 @@ export const Navbar = () => {
                         <CustomIcon name="darkIcon" size={24} />
                     </div>
                     <LanguageSwitcherDropdown />
-                    <button
-                        type="button"
-                        className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 text-center uppercase hover:scale-110 transform-gpu transition-transform duration-300 ease-in-out"
-                        onClick={() => setModalOpen(true)}
-                    >
-                        {t('contact')}
-                    </button>
                 </div>
             </nav>
             <ContactFormModal open={modalOpen} onOpenChange={setModalOpen} />
